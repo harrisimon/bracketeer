66d6dd69e6823d47ce29d91d53f18f527d32d04c
harrisimon <harrisimon@gmail.com>
2023-04-10 09:58:39 -0400

    added bracket node and leaf components with interface and types

---
+++ src/components/BracketNode.tsx
@@ -0,0 +1,26 @@
+import BracketLeaf from "./BracketLeaf"
+
+interface NodeProps {
+	data: {
+		left: {
+			name: string
+			votes: number
+		}
+		right: {
+			name: string
+			votes: number
+		}
+	}
+}
+const BracketNode = (props: NodeProps) => {
+	const { data } = props
+	console.log(data)
+	return (
+		<div>
+			<BracketLeaf name={data.left.name} votes={data.left.votes} />
+			<BracketLeaf name={data.right.name} votes={data.right.votes} />
+		</div>
+	)
+}
+
+export default BracketNode
