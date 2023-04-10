8218269e9db8c4ea793820619001c0405c59053c
Jonah DH <jdh@jonahs-air.mynetworksettings.com>
2023-04-10 18:39:32 -0400

    include trees of different sizes in tests

--- __tests__/makeTree.ts
+++ __tests__/makeTree.ts
@@ -1,5 +1,5 @@
 import makeTree from '../src/util/makeTree';
-import { propCheck, maxDepth, depthWrapper } from '../src/util/helpers';
+import { propCheck, depthWrapper } from '../src/util/helpers';
 
 describe('makeTree', () => {
   const list4 = ['one', 'two', 'three', 'four'];
@@ -32,23 +32,26 @@ describe('makeTree', () => {
     'fifteen',
     'sixteen',
   ];
-  const tree = makeTree(list8);
+  const tree4 = makeTree(list4);
+  const tree8 = makeTree(list8);
 
   describe('convert list of contestants into binary tree', () => {
     it('returns an object of BracketType', () => {
-      expect(tree).toHaveProperty('contestant');
-      expect(tree).toHaveProperty('votes');
-      expect(tree).toHaveProperty('left');
-      expect(tree).toHaveProperty('right');
-      expect(tree).toHaveProperty('round');
+      expect(tree4).toHaveProperty('contestant');
+      expect(tree4).toHaveProperty('votes');
+      expect(tree4).toHaveProperty('left');
+      expect(tree4).toHaveProperty('right');
+      expect(tree4).toHaveProperty('round');
     });
 
     it('has depth equal to 1 + log2 of string length', () => {
-      expect(depthWrapper.depth(tree)).toEqual(4);
+      expect(depthWrapper.depth(tree4)).toEqual(3);
+      expect(depthWrapper.depth(tree8)).toEqual(4);
     });
 
     it('has undefined contestant property everywhere but round 1', () => {
-      expect(propCheck(tree));
+      expect(propCheck(tree4));
+      expect(propCheck(tree8));
     });
 
     // it('throws an error if the list of contestants does not contain a number of items that is a power of two', () => {
