9acaddb0a313275c8991ec72c79607f0b6fef072
Jonah DH <jdh@jonahs-air.mynetworksettings.com>
2023-04-11 08:37:58 -0400

    use pop rather than shift in makeTree

--- src/util/makeTree.ts
+++ src/util/makeTree.ts
@@ -8,7 +8,7 @@ const bracketPartWrapper = {
   bracketPart: (contestants: string[] | null, round: number) => {
     // only add contestant names to the outermost columns / round 1
     const contestant: string | undefined =
-      round === 1 ? contestants?.shift() : undefined;
+      round === 1 ? contestants?.pop() : undefined;
 
     const left: BracketType | null =
       round === 1
@@ -31,12 +31,14 @@ const bracketPartWrapper = {
 };
 
 const makeBracket = (contestants: string[]) => {
+  // reverse the array so we can pop rather than shift as we add elements
+  contestants = contestants.reverse();
   // add one to account for the top node of the tree
   // for now discard extras beyond power of 2
   let rounds = Math.floor(Math.log2(contestants.length)) + 1;
   return bracketPartWrapper.bracketPart(contestants, rounds);
 };
 
-// console.log(makeBracket(['one', 'two', 'three', 'four']));
+console.log(makeBracket(['one', 'two', 'three', 'four']));
 
 export default makeBracket;
