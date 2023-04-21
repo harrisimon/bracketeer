b100a1e1d939179a80fde1902c038f2f05fc4fef
harrisimon <harrisimon@gmail.com>
2023-04-21 15:59:25 -0400

    barebones server mongo setup

--- src/server/config/db.ts
+++ src/server/config/db.ts
@@ -1,16 +1,17 @@
-'use strict'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const mongooseBaseName = 'bracketeer'
 
 const database = {
-    development: `mongodb://localhost/${mongooseBaseName}-development`,
-    test: `mongodb://localhost/${mongooseBaseName}-test`
+	development: `mongodb://localhost/${mongooseBaseName}-development`,
+	test:  `mongodb://localhost/${mongooseBaseName}-test`,
 }
-
-// Identify environment
 
 const localDb = process.env.TESTENV ? database.test : database.development
 
+
 const currentDb = process.env.MONGODB_URI || localDb
 
-module.exports = currentDb
+export default currentDb
