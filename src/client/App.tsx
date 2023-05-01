2b0b749723ef2f0e2ca8b28c21e9a2b02aff5a9e
harrisimon <harrisimon@gmail.com>
2023-05-01 09:15:41 -0400

    removed vite and react logos

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import BracketLeaf from './../client/components/BracketLeaf';
 import './App.css';
 import BracketNode from './../client/components/BracketNode';
