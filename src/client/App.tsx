a0436b949b343bec7e3b8d96fe2bb7fb39883023
harrisimon <harrisimon@gmail.com>
2023-05-17 10:08:23 -0400

    added react-router, initial setup, creating bracket currently navigates back to home route

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,32 +1,38 @@
-import { useState } from 'react';
-import BracketLeaf from './../client/components/BracketLeaf';
-import './App.css';
-import BracketNode from './../client/components/BracketNode';
-import Input from './components/Input';
-import CreateBracket from './components/CreateBracket';
+import { Fragment, useState } from "react"
+import { HashRouter as Router, Route, Link, Routes } from "react-router-dom"
+import BracketLeaf from "./../client/components/BracketLeaf"
+import "./App.css"
+import BracketNode from "./../client/components/BracketNode"
+import Input from "./components/Input"
+import CreateBracket from "./components/CreateBracket"
+import Home from "./components/pages/home"
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0)
 
-  const data = {
-    left: {
-      name: 'Red Dead Redemption 2',
-      votes: 4,
-    },
-    right: {
-      name: 'Ocarina of Time',
-      votes: 6,
-    },
-  };
+	const data = {
+		left: {
+			name: "Red Dead Redemption 2",
+			votes: 4,
+		},
+		right: {
+			name: "Ocarina of Time",
+			votes: 6,
+		},
+	}
 
-  return (
-    <div className='App'>
-      <h3>App</h3>
-      {/* <Input /> */}
-      <CreateBracket />
-      {/*<BracketNode data={data}/>*/}
-    </div>
-  );
+	return (
+		<Fragment>
+			<div className="App">
+				<h3>Bracketeer</h3>
+      {/* Insert nav here later */}
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/create" element={<CreateBracket />} />
+				</Routes>
+			</div>
+		</Fragment>
+	)
 }
 
-export default App;
+export default App
