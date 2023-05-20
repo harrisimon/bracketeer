5bfeffca7dd708f523c20ef6f581b1a7e24b63b1
Jonah DH <jdh@jonahs-air.mynetworksettings.com>
2023-05-19 22:19:53 -0400

    add react-xarrows

--- src/client/components/RoundColumn.tsx
+++ src/client/components/RoundColumn.tsx
@@ -1,20 +1,28 @@
+import Xarrow from 'react-xarrows';
 import testTournamentData from '../../assets/test_data/test-tournament';
 
 export interface RoundColumnProps {
-  roundData: (typeof testTournamentData.matchUps)[];
+  roundData: typeof testTournamentData.matchUps;
 }
 
 const RoundColumn = (props: RoundColumnProps) => {
   const { roundData } = props;
-
+  console.log('rounddata: ', roundData);
   // eventually replace contestant containers with their own JSX component
   return (
     <div className='round-column'>
       {roundData.map((el) => {
         return (
-          <div className='matchup-container'>
-            <div className='contestant-container'>{Object.keys(el)[0]}</div>
-            <div className='contestant-container'>{Object.keys(el)[1]}</div>
+          <div className='matchup-container' id={`matchup${el.matchNumber}`}>
+            <div className='contestant-container'>{el.matchNumber}</div>
+            <div className='contestant-container'>{el.matchNumber}</div>
+            {el.next && (
+              <Xarrow
+                start={`matchup${el.matchNumber}`}
+                end={`matchup${el.next}`}
+                path='grid'
+              />
+            )}
           </div>
         );
       })}
