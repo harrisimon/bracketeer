ea87190f69958ed89ceb4f38a6ca37980a2e29bf
Jonah DH <jdh@Jonahs-MacBook-Air.local>
2023-05-24 18:38:23 -0400

    commit before revert

--- src/client/components/RoundColumn.tsx
+++ src/client/components/RoundColumn.tsx
@@ -1,14 +1,32 @@
 import Xarrow from 'react-xarrows';
+import { useEffect, ReactElement } from 'react';
 import testTournamentData from '../../assets/test_data/test-tournament';
 
 export interface RoundColumnProps {
   roundData: typeof testTournamentData.matchUps;
+  unidirectional: boolean;
 }
 
 const RoundColumn = (props: RoundColumnProps) => {
-  const { roundData } = props;
+  const { roundData, unidirectional } = props;
   roundData.sort((a, b) => a.matchNumber - b.matchNumber);
   console.log('rounddata: ', roundData);
+
+  const arrows: ReactElement[] = [];
+
+  useEffect(() => {
+    roundData.forEach((el) => {
+      if (el.next)
+        arrows.push(
+          <Xarrow
+            start={`matchup${el.matchNumber}`}
+            end={`matchup${el.next}`}
+            path='grid'
+          />
+        );
+    });
+  }, [unidirectional]);
+
   // eventually replace contestant containers with their own JSX component
   return (
     <div className='round-column'>
@@ -29,13 +47,7 @@ const RoundColumn = (props: RoundColumnProps) => {
             <div className='contestant-container'>
               {el.matchNumber} next={`matchup${el.next}`}
             </div>
-            {el.next && (
-              <Xarrow
-                start={`matchup${el.matchNumber}`}
-                end={`matchup${el.next}`}
-                path='grid'
-              />
-            )}
+            <div>{arrows}</div>
           </div>
         );
       })}
