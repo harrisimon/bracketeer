2a5cf4035edc5c115d6809c0a41d4f43d2598259
Jonah DH <jdh@Jonahs-MacBook-Air.local>
2023-05-24 19:33:57 -0400

    clean up styling, improve comments

--- src/client/components/Bracket.tsx
+++ src/client/components/Bracket.tsx
@@ -2,12 +2,9 @@ import { useState, useEffect } from 'react';
 import testTournamentData from '../../assets/test_data/test-tournament';
 import RoundColumn from './RoundColumn';
 
-//typing will have to change once I replace dummy data with real API calls
+//typing will have to change once dummy data replaced with real API calls
 // eventually pass tournament ID into Bracket
 // use it to useQuery and set state
-
-// grid-template-areas better?
-
 export interface matchUpRenderObjectTEST {
   [k: string]: (typeof testTournamentData.matchUps)[];
 }
@@ -15,19 +12,20 @@ export interface matchUpRenderObjectTEST {
 const Bracket = () => {
   const [matchUps, setMatchUps] = useState<matchUpRenderObjectTEST>({});
   const [unidirectional, setUnidirectional] = useState<boolean>(true);
-  const [numberOfRounds, setNumberOfRounds] = useState(
+
+  // the number of columns is based on how many matchups there are, and whether the view is unidirectional or not
+  const [numberOfColumns, setNumberOfColumns] = useState(
     unidirectional
       ? Math.log2(testTournamentData.matchUps.length + 1)
       : Math.log2(testTournamentData.matchUps.length + 1) * 2 - 1
   );
 
-  // get number of rounds
-  // use it to set number of rows/cols
+  // create matchUps object whose keys are round numbers and whose values are the array of matchups for each column
   useEffect(() => {
     const matchUpData: matchUpRenderObjectTEST = {};
     if (unidirectional) {
-      setNumberOfRounds(Math.log2(testTournamentData.matchUps.length + 1));
-      for (let i = 1; i <= numberOfRounds; i++) {
+      setNumberOfColumns(Math.log2(testTournamentData.matchUps.length + 1));
+      for (let i = 1; i <= numberOfColumns; i++) {
         const key = 'round' + i.toString();
         matchUpData[key] = [];
         const matchUpsFromRound = testTournamentData.matchUps.filter(
@@ -39,7 +37,7 @@ const Bracket = () => {
       // store new number of columns in block-scoped variable for use in for loop. We can't rely on state being updated immediately
       const newNumberOfColumns =
         Math.log2(testTournamentData.matchUps.length + 1) * 2 - 1;
-      setNumberOfRounds(newNumberOfColumns);
+      setNumberOfColumns(newNumberOfColumns);
 
       for (let i = 1; i < newNumberOfColumns; i++) {
         const matchUpsFromRound = testTournamentData.matchUps.filter(
@@ -51,9 +49,7 @@ const Bracket = () => {
         const middleIndex = Math.ceil(matchUpsFromRound.length / 2);
         const left = matchUpsFromRound.slice(0, middleIndex);
         const right = matchUpsFromRound.slice(middleIndex);
-        if (matchUpsFromRound.length === 1) {
-          console.log('LENGTH CHECK', left, right);
-        }
+
         if (left.length) {
           let key = `lRound${i.toString()}`;
           matchUpData[key] = [];
@@ -66,7 +62,6 @@ const Bracket = () => {
         }
       }
     }
-    console.log('mu: ', matchUpData);
     setMatchUps(matchUpData);
   }, [unidirectional]);
 
@@ -77,24 +72,20 @@ const Bracket = () => {
         style={
           unidirectional
             ? {
-                gridTemplateColumns: `repeat(${numberOfRounds}, 1fr)`,
+                gridTemplateColumns: `repeat(${numberOfColumns}, 1fr)`,
                 columnGap: '10%',
               }
             : {
-                gridTemplateColumns: `repeat(${numberOfRounds}, 1fr)`,
+                gridTemplateColumns: `repeat(${numberOfColumns}, 1fr)`,
                 columnGap: '5%',
               }
         }
       >
         {Object.keys(matchUps)
+          // if view isn't unidirectional, sort matchups to left and right according to their position
           .sort((a, b) => (a[0] === 'l' ? 1 : -1))
           .map((round) => {
-            return (
-              <RoundColumn
-                roundData={matchUps[round]}
-                unidirectional={unidirectional}
-              />
-            );
+            return <RoundColumn roundData={matchUps[round]} />;
           })}
       </div>
       <button onClick={() => setUnidirectional(!unidirectional)}>
--- src/client/components/RoundColumn.tsx
+++ src/client/components/RoundColumn.tsx
@@ -5,34 +5,25 @@ import { useEffect } from 'react';
 
 export interface RoundColumnProps {
   roundData: typeof testTournamentData.matchUps;
-  unidirectional: boolean;
 }
 
 const RoundColumn = (props: RoundColumnProps) => {
-  const { roundData, unidirectional } = props;
+  const { roundData } = props;
 
   const updateXarrow = useXarrow();
 
+  // when roundData changes, redraw arrows based on new positions of matchup divs
   useEffect(() => {
-    console.log('attempt to update arrows');
     updateXarrow();
   }, [roundData]);
 
   roundData.sort((a, b) => a.matchNumber - b.matchNumber);
-  // eventually replace contestant containers with their own JSX component
+
   return (
     <div className='round-column'>
       {roundData.map((el) => {
         return (
-          <div
-            className='matchup-container'
-            id={`matchup${el.matchNumber}`}
-            style={
-              el.contestant1
-                ? { background: 'lavender' }
-                : { background: 'white' }
-            }
-          >
+          <div className='matchup-container' id={`matchup${el.matchNumber}`}>
             <div className='contestant-container'>
               {el.matchNumber} next={`matchup${el.next}`}
             </div>
